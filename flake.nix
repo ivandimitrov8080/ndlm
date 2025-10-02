2ea8752787e577c29965a8ac94c0c4bafb39ac87
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2025-10-02 23:25:41 +0200

    tests are building

--- flake.nix
+++ flake.nix
@@ -7,77 +7,75 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-      lib = pkgs.lib;
       ndlm = (
         pkgs.rustPlatform.buildRustPackage rec {
           pname = "ndlm";
           version = "1.0";
-           src = ./.;
-           cargoHash = "sha256-iA8qkIrXJ3hf4V34MLGwt4yChkmjpo67oQvTJf5R+uw=";
- 
-           meta = {
-             mainProgram = pname;
-           };
-         }
-       );
-       test-vm = { pkgs, lib, ndlm, ... }: {
-         nixpkgs.hostPlatform = "x86_64-linux";
-         system.stateVersion = "23.11";
-         programs.sway.enable = true;
-         services.greetd = {
-           enable = true;
-           settings = {
-             default_session = {
-               command = lib.mkForce "${ndlm}/bin/ndlm --session ${pkgs.sway}/bin/sway --theme-file ${(pkgs.catppuccin-plymouth.override { variant = "mocha"; })}/share/plymouth/themes/catppuccin-mocha/catppuccin-mocha.plymouth";
-               user = "greeter";
-             };
-           };
-         };
-         users.users = {
-           test = {
-             isNormalUser = true;
-             password = "test";
-           };
-           greeter = {
-             extraGroups = [
-               "video"
-               "input"
-               "render"
-             ];
-           };
-         };
-         fileSystems."/" = {
-           device = "/dev/vda";
-           fsType = "ext4";
-         };
-         boot.loader.grub.devices = [ "/dev/vda" ];
-       };
-     in
-     {
-       nixosConfigurations.default = nixpkgs.lib.nixosSystem {
-         modules = [
-           test-vm
-           {
-             _module.args = { inherit lib ndlm; };
-           }
-         ];
-       };
-       packages.${system}.default = self.nixosConfigurations.default.config.system.build.vm;
-       checks.${system}.default = self.nixosConfigurations.default.config.system.build.vm;
-       nixosTests.default = {
-         nodes.machine = { ... }: {
-           imports = [
-             test-vm
-             {
-               _module.args = { inherit lib ndlm; };
-             }
-           ];
-         };
-         testScript = ''
-           start_all;
-           machine.wait_for_unit("greetd.service");
-           machine.succeed("pgrep ndlm");
-         '';
-       };
+          src = ./.;
+          cargoHash = "sha256-iA8qkIrXJ3hf4V34MLGwt4yChkmjpo67oQvTJf5R+uw=";
+          meta = {
+            mainProgram = pname;
+          };
+        }
+      );
+      test-vm =
+        { pkgs, lib, ... }:
+        {
+          system.stateVersion = "23.11";
+          programs.sway.enable = true;
+          services.greetd = {
+            enable = true;
+            settings = {
+              default_session = {
+                command = lib.mkForce "${ndlm}/bin/ndlm --session ${pkgs.sway}/bin/sway --theme-file ${
+                  (pkgs.catppuccin-plymouth.override { variant = "mocha"; })
+                }/share/plymouth/themes/catppuccin-mocha/catppuccin-mocha.plymouth";
+                user = "greeter";
+              };
+            };
+          };
+          users.users = {
+            test = {
+              isNormalUser = true;
+              password = "test";
+            };
+            greeter = {
+              extraGroups = [
+                "video"
+                "input"
+                "render"
+              ];
+            };
+          };
+          fileSystems."/" = {
+            device = "/dev/vda";
+            fsType = "ext4";
+          };
+          boot.loader.grub.devices = [ "/dev/vda" ];
+        };
+    in
+    {
+      nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+        modules = [
+          test-vm
+          {
+            nixpkgs.hostPlatform = "x86_64-linux";
+          }
+        ];
+      };
+      packages.${system}.default = self.nixosConfigurations.default.config.system.build.vm;
+      checks.${system}.default = pkgs.testers.runNixOSTest {
+        name = "test";
+        nodes = {
+          machine = test-vm;
+        };
+        testScript =
+          #py
+          ''
+            start_all;
+            machine.wait_for_unit("greetd.service");
+            machine.succeed("pgrep ndlm");
+          '';
+      };
     };
 }
