311e2e97e14ccacc33f22a0c320b45a393e2feed
Ivan Kirilov Dimitrov <ivan@idimitrov.dev>
2026-03-04 20:50:24 +0100

    removing eval warning for pre-commit hooks

--- flake.nix
+++ flake.nix
@@ -115,15 +115,15 @@
                   pango.dev
                   cargo-edit
                 ];
-                git-hooks.hooks = {
-                  nixfmt.enable = true;
-                  deadnix.enable = true;
-                  statix.enable = true;
-                  rustfmt.enable = true;
-                  rustfmt.settings.config = {
-                    edition = "2024";
-                  };
-                };
+                # git-hooks.hooks = {
+                #   nixfmt.enable = true;
+                #   deadnix.enable = true;
+                #   statix.enable = true;
+                #   rustfmt.enable = true;
+                #   rustfmt.settings.config = {
+                #     edition = "2024";
+                #   };
+                # };
               }
             ];
           };
